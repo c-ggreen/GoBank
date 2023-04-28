fcf395065170fc8af1449e1a0f5e0725f9e6c6f3
c-ggreen <chadggreen@gmail.com>
2023-04-27 20:47:55 -0400

    cleaing up imports

--- GoBankDB/src/main/java/com/chadwick/GoBankDB/service/CustomerService.java
+++ GoBankDB/src/main/java/com/chadwick/GoBankDB/service/CustomerService.java
@@ -14,9 +14,7 @@ import com.chadwick.GoBankDB.repository.AccountRepository;
 import com.chadwick.GoBankDB.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.List;
